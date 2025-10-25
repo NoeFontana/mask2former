35f1be26e2ddaed6bfc728b39baa9d910e59329c
Noé Fontana <noe.fontana.pro@gmail.com>
2025-10-25 14:43:02 -0400

    feat: Add Feed-Forward Network module

--- src/mask2former/modeling/decoder.py
+++ src/mask2former/modeling/decoder.py
@@ -168,3 +168,24 @@ class SelfAttention(nn.Module):
         )
 
         return self.out_proj(attn_out) + query_features
+
+
+class FFN(nn.Module):
+    """Feed-Forward Network module.
+
+    This module implements the Feed-Forward Network of the transformer decoder module.
+
+    Args:
+        embedding_dim (int): Dimension of the embedding space for attention computation.
+        hidden_dim (int): Dimension of the hidden layer.
+    """
+
+    def __init__(self, embedding_dim: int, hidden_dim: int) -> None:
+        super().__init__()
+
+        self.linear_1 = nn.Linear(in_features=embedding_dim, out_features=hidden_dim)
+        self.activation = nn.GELU()
+        self.linear_2 = nn.Linear(in_features=hidden_dim, out_features=embedding_dim)
+
+    def forward(self, queries: torch.Tensor) -> torch.Tensor:
+        return self.linear_2(self.activation(self.linear_1(queries)))
