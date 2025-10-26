058adf5aedfe151c3b1479528b0204030c396359
Noé Fontana <noe.fontana.pro@gmail.com>
2025-10-26 11:20:57 -0400

    feat: implement parameter initialization for FFN and MLP classes

--- src/mask2former/modeling/common/ffn.py
+++ src/mask2former/modeling/common/ffn.py
@@ -16,8 +16,18 @@ class FFN(nn.Module):
         super().__init__()
 
         self.linear_1 = nn.Linear(in_features=embedding_dim, out_features=hidden_dim)
-        self.activation = nn.GELU()
         self.linear_2 = nn.Linear(in_features=hidden_dim, out_features=embedding_dim)
+
+        self.activation = nn.GELU()
+
+        self._reset_parameters()
+
+    def _reset_parameters(self) -> None:
+        """Initialize parameters."""
+        nn.init.xavier_uniform_(self.linear_1.weight)
+        nn.init.constant_(self.linear_1.bias, 0.0)
+        nn.init.xavier_uniform_(self.linear_2.weight)
+        nn.init.constant_(self.linear_2.bias, 0.0)
 
     def forward(self, queries: torch.Tensor) -> torch.Tensor:
         return self.linear_2(self.activation(self.linear_1(queries)))
@@ -30,9 +40,19 @@ class MLP(nn.Module):
         super().__init__()
 
         self.linear_1 = nn.Linear(in_features=embedding_dim, out_features=hidden_dim)
-        self.activation = nn.ReLU()
         self.linear_2 = nn.Linear(in_features=hidden_dim, out_features=hidden_dim)
         self.linear_3 = nn.Linear(in_features=hidden_dim, out_features=embedding_dim)
+
+        self.activation = nn.ReLU()
+
+    def _reset_parameters(self) -> None:
+        """Initialize parameters."""
+        nn.init.xavier_uniform_(self.linear_1.weight)
+        nn.init.constant_(self.linear_1.bias, 0.0)
+        nn.init.xavier_uniform_(self.linear_2.weight)
+        nn.init.constant_(self.linear_2.bias, 0.0)
+        nn.init.xavier_uniform_(self.linear_3.weight)
+        nn.init.constant_(self.linear_3.bias, 0.0)
 
     def forward(self, query_features: torch.Tensor) -> torch.Tensor:
         """query_features: (batch_size, num_queries, embedding_dim)
